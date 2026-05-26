93fc12c4fb7d16376e99d4e18f9d6f98b1bf35b2
abidazis <abidathanandaazis@gmail.com>
2026-05-26 10:41:05 +0700

    fix: save product kembali

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,5 +1,6 @@
 // src/sanityClient.js
 import { createClient } from "@sanity/client";
+// Ganti import lama dengan yang baru ini:
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
@@ -10,6 +11,7 @@ export const client = createClient({
   token: "skGWgmtKjFkUNE91ql586q0P9mxzkx4bM0fsscjaWq1ClG5bujpzVWHbgIbrbX7EeJ7bwDebrQXJ5XHsI7XJCWyxgKaMQn7pCRZNao3jSSxqHOPoqPsA5qhL549eSJOCwZs2wl5Oi4cLaSYEE4WWfNGpHA57jDd9iHYBDrxlvfzxki84fW17",
 });
 
+// Tetap sama
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => {
   if (!source) return { url: () => "" };
